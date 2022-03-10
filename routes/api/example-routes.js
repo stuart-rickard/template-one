2b9167b8683062ba21e83a7420b042feb5f276a7
stuart-rickard <stuart@bau-dev.com>
2022-03-09 21:23:06 -0800

    html and api/example routes updates

--- routes/api/example-routes.js
+++ routes/api/example-routes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-const { newData } = require("../../controllers/example-controller");
+// const { newData } = require("../../controllers/example-controller");
+const newData = function () {
+  console.log("inside newData");
+};
 
 // /api/example
-router.route("/example").post(newData);
+// router.route("/example").post(newData);
+router.route("/").get((req, res) => {
+  res.send("<h1>hello</h1>");
+  // res.sendFile(path.join(__dirname, "../../public/home.html"));
+});
 
 // // /api/comments/<pizzaId>/<commentId>
 // router
